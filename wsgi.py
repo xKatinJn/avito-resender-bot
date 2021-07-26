af615cc98138edc264f37f0290def7341f21f10c
xKatinJn <antosha.katin@yandex.ru>
2021-07-26 18:59:45 +0500

    bot sends avito msg

--- wsgi.py
+++ wsgi.py
@@ -21,7 +21,7 @@ def notifications_webhook():
     active_chat_id = [chat['chat_id'] for chat in Chat.get_all_chats() if chat['is_active']]
     if active_chat_id:
         data = request.json
-        text = data['messages']['content']['text']
+        text = data['payload']['value']['content']['text']
         print(data)
         bot.send_message(active_chat_id[0], localization.messages_loc['bot_new_msg'].format(text))
     payload = {'ok': True}
