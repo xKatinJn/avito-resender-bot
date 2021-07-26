4f8a8dbb5efa76abdeadaf5665a1a0ea451a79bf
xKatinJn <antosha.katin@yandex.ru>
2021-07-26 19:06:27 +0500

    bot reacts to ping messages

--- bot.py
+++ bot.py
@@ -17,10 +17,11 @@ bot = Bot(token=API_TOKEN)
 def message_handler(update: Update, context: CallbackContext) -> None:
     text = update.message.text
     current_chat = update.message.chat
+    bot_info = bot.get_me()
     print(f'User msg is: "{text}"')
     if current_chat['type'] in ['group', 'supergroup']:
         chat_id = int(current_chat['id'])
-        if text == '/start':
+        if text in ['/start', f'/start@{bot_info.username}']:
             for chat in Chat.get_all_chats():
                 print(f'CHAT = {chat}')
                 if chat['is_active']:
@@ -28,7 +29,7 @@ def message_handler(update: Update, context: CallbackContext) -> None:
             new_active_chat = Chat(chat_id=chat_id, is_active=True)
             new_active_chat.save()
             bot.send_message(chat_id, messages_loc['bot_started'])
-        elif text == '/end':
+        elif text in ['/end', f'/end@{bot.username}']:
             for chat in Chat.get_all_chats():
                 if chat['is_active'] and chat['chat_id'] == chat_id:
                     Chat.delete_by_id(chat['id'])
